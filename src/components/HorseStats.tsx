d230e2f5b41425169d6b4434bed0e98c4217761b
Kira <kira.eva@protonmail.com>
2025-09-07 11:54:19 +0100

    main stat table

--- src/components/HorseStats.tsx
+++ src/components/HorseStats.tsx
@@ -5,5 +5,21 @@ export function HorseStats() {
         <div class="horse-portrait">
             <img src="/src/assets/icons/chara/chr_icon_1001.png" />
         </div>
+        <div class="stats-container">
+            <Stat title={"Speed"} value={22} />
+            <Stat title={"Stamina"} />
+            <Stat title={"Power"} />
+            <Stat title={"Guts"} />
+            <Stat title={"Wit"} />
+        </div>
     </div>)
+}
+
+function Stat(props) {
+    return <div class="stat">
+        <div class="stat-header">
+            {props.title}
+        </div>
+        <div class="stat-value">{props.value ?? 0}</div>
+    </div>
 }
