4da675ac0b2dbd8378f1088838e155139a5944ed
Kira <kira.eva@protonmail.com>
2025-09-07 12:19:34 +0100

    use input for stats

--- src/components/HorseStats.tsx
+++ src/components/HorseStats.tsx
@@ -1,18 +1,24 @@
 import "./HorseStats.css"
 
 export function HorseStats() {
-    return (<div class="horse-stats">
-        <div class="horse-portrait">
-            <img src="/src/assets/icons/chara/chr_icon_1001.png" />
-        </div>
-        <div class="stats-container">
-            <Stat title={"Speed"} value={22} />
-            <Stat title={"Stamina"} />
-            <Stat title={"Power"} />
-            <Stat title={"Guts"} />
-            <Stat title={"Wit"} />
-        </div>
-    </div>)
+    return (
+        <div class="horse-stats">
+            <div class="horse-bio">
+                <div class="horse-portrait">
+                    <img src="/src/assets/icons/chara/chr_icon_1001.png" />
+                </div>
+                <div class="horse-name">
+                    Goldship
+                </div>
+            </div>
+            <div class="stats-container">
+                <Stat title={"Speed"} value={22} />
+                <Stat title={"Stamina"} />
+                <Stat title={"Power"} />
+                <Stat title={"Guts"} />
+                <Stat title={"Wit"} />
+            </div>
+        </div>)
 }
 
 function Stat(props) {
@@ -20,6 +26,7 @@ function Stat(props) {
         <div class="stat-header">
             {props.title}
         </div>
-        <div class="stat-value">{props.value ?? 0}</div>
+        <input class="stat-value" type="number" min="1" max="1200" value={props.value ?? 0} />
+        {/* <div class="stat-value">{props.value ?? 0}</div> */}
     </div>
 }
